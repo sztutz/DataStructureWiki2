5ce118087204509a8e47c9eb3e10518428ec9ee6
sztutz <emailofstuart@gmail.com>
2022-10-12 12:14:06 +0800

    GUI created

---
+++ DataStructure.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureWiki2
+{
+    internal class DataStructure : IComparable<DataStructure>
+    {
+        private string Name;
+        private string Category;
+        private string Structure;
+        private string Definition;
+
+        public int CompareTo(DataStructure other)
+        {
+            // The data structure comparison depends on the comparison of the String name values.
+            return Name.CompareTo(other.Name);
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+
+        public string GetCategory()
+        {
+            return Category;
+        }
+
+        public void SetCategory(string category)
+        {
+            Category = category;
+        }
+
+        public string GetStructure()
+        {
+            return Structure;
+        }
+
+        public void SetStructure(string structure)
+        {
+            Structure = structure;
+        }
+
+        public string GetDefinition()
+        {
+            return Definition;
+        }
+
+        public void SetDefinition(string definition)
+        {
+            Definition = definition;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace DataStructureWiki2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main());
         }
     }
 }
